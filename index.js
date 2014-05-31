b5bd31324a88fb050b481b2c33f3865f3b96cf27
Will Scott <willscott@gmail.com>
2014-05-31 13:02:53 -0700

    express 4.0, routes, templates

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
 var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
